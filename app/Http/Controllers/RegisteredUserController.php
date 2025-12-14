2dee5c93151734738c57249d23251b7be6bb921c
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-14 10:36:18 +0800

    commit

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,32 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rules\Password;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class RegisteredUserController extends Controller
 {
-    public function create()
-    {
-        return view('auth.register');
-    }
-    public function store(Request $request)
-    {
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => ['required', 'confirmed', Password::defaults()],
-        ]);
-        $user = User::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'password' => Hash::make($validated['password']),
-        ]);
 
-        Auth::login($user);
-
-        return redirect()->route('forms.index');
-    }
 }
--- resources/views/forms/index.blade.php
+++
@@ -1,6 +0,0 @@
-this is the index
-
-<form method="POST" action=" {{ route('logout') }} ">
-@csrf
-<button type="submit">Logout</button>
-</form>
