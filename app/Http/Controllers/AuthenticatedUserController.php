8a4f0548e83f67a8f5c11d71fb332c61051c86c9
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-14 13:33:49 +0800

    commit

--- app/Http/Controllers/AuthenticatedUserController.php
+++ app/Http/Controllers/AuthenticatedUserController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedUserController extends Controller
 {
-
+    public function create()
+    {
+        return view('auth.login');
+    }
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended(route('forms.index'));
+        }
+        return back()->withErrors([
+            'email' => 'The credentials do not match our records',
+        ]);
+    }
+    public function destroy(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login');
+    }
 }
