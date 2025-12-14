cba3507b618bbb65fe40c6fe7700377a3264a40e
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-14 11:20:21 +0800

    commit

--- app/Http/Controllers/AuthenticatedUserController.php
+++ app/Http/Controllers/AuthenticatedUserController.php
@@ -7,13 +7,26 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedUserController extends Controller
 {
-    public function create() {
-        return view ('auth.login');
+    public function create()
+    {
+        return view('auth.login');
     }
-    public function store() {
-
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended(route('forms.index'));
+        }
+        return back()->withErrors([
+            'email' => 'The credentials does not match our recored',
+        ]);
     }
-    public function destroy(Request $request) {
+    public function destroy(Request $request)
+    {
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
