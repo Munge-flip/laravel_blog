d0fd29341628b177f97bdb8ddccb12ac3223140e
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-14 11:37:14 +0800

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,7 @@
-<form method="POST" action=" {{ route('login') }} ">
-    @csrf
+<form action="">
     <h3>Login</h3>
     <div>
-        <label for="login-email">Email</label><br>
-        <input type="email" value=" {{ old('email') }} " name="email" required><br>
-        @error('email')
-        <div style="color: red"> {{$message}} </div>
-        @enderror
+        <label for="">Email</label><br>
+        <input type="email" name="email" required>
     </div>
-    <div>
-        <label for="login-password">Password</label><br>
-        <input type="password" name="password" required><br>
-        @error('password')
-        <div style="color: red"> {{$message}} </div>
-        @enderror
-    </div>
-    <button type="submit">Login</button>
-    <a href=" {{ route('register') }} ">Don't have an account yet? Register</a>
 </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,31 +1,7 @@
-<form method="POST" action=" {{ route('register') }} ">
-    @csrf
+<form action="">
     <h3>Register</h3>
     <div>
-        <label for="reg-name">Name</label><br>
-        <input type="text" name="name" value=" {{ old('name') }} " required><br>
-        @error('name')
-        <div style="color: red"> {{ $message }} </div>
-        @enderror
+        <label for="">Name</label><br>
+        <input type="text" name="name" required>
     </div>
-    <div>
-        <label for="reg-email">Email</label><br>
-        <input type="email" name="email" value=" {{ old('email') }} " required><br>
-        @error('email')
-        <div style="color: red"> {{ $message }} </div>
-        @enderror
-    </div>
-    <div>
-        <label for="reg-password">Password</label><br>
-        <input type="password" name="password" required><br>
-        @error('password')
-        <div style="color: red"> {{ $message }} </div>
-        @enderror
-    </div>
-    <div>
-        <label for="reg-confirm">Confirm Password</label><br>
-        <input type="password" name="password_confirmation" required><br>
-    </div>
-    <button type="submit">Register</button>
-    <a href=" {{ route('login') }} ">Already have an Account?</a>
 </form>
