5e4bc3b5fd8935b74a8636f161cf2a44b7bba8a9
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-14 12:25:43 +0800

    commit

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -6,7 +6,4 @@ use Illuminate\Http\Request;
 
 class FormController extends Controller
 {
-    public function index() {
-        return view ('forms.index');
-    }
 }
--- resources/views/forms/index.blade.php
+++ resources/views/forms/index.blade.php
@@ -1,6 +1,2 @@
 this is the index
 
-<form method="POST" action=" {{ route('logout') }} ">
-    @csrf
-    <button type="submit">Logout</button>
-</form>
