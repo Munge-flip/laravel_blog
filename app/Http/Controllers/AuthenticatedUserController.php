08accf130d2180605c4b3a75dcf5873e9e5a2575
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-14 21:31:33 +0800

    commit

--- app/Http/Controllers/AuthenticatedUserController.php
+++ app/Http/Controllers/AuthenticatedUserController.php
@@ -11,8 +11,18 @@ class AuthenticatedUserController extends Controller
     {
         return view('auth.login');
     }
-    public function store() {
-
+    public function store(Request $request) {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended(route('forms.index'));
+        }
+        return back()->withErrors([
+            'email' => 'The credentials do not match our records',
+        ]);
     }
     public function destroy(Request $request) {
         Auth::logout();
