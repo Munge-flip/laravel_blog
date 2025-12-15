ed43740fbecaabe8a609da8bc1c1c3e712535cbb
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-15 19:21:43 +0800

    commit

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -10,23 +10,5 @@ use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
-    public function create()
-    {
-        return view('auth.register');
-    }
-    public function store(Request $request)
-    {
-        $validated = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Password::defaults()],
-        ]);
-        $user = User::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'password' => Hash::make($validated['password']),
-        ]);
-        Auth::login($user);
-        return redirect()->route('forms.index');
-    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,4 @@
 <?php
 
-use App\Http\Controllers\AuthenticatedUserController;
-use App\Http\Controllers\FormController;
-use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
-    Route::post('/register', [RegisteredUserController::class, 'store']);
-
-    Route::get('/login', [AuthenticatedUserController::class, 'create'])->name('login');
-    Route::post('/login', [AuthenticatedUserController::class, 'store']);
-});
-
-Route::middleware('auth')->group(function () {
-    Route::post('/logout', [AuthenticatedUserController::class, 'destroy'])->name('logout');
-    Route::resource('forms', FormController::class);
-});
