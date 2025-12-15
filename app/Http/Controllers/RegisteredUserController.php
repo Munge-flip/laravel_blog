75cb5f60d6dfc65284d44bbf011285f85ff18c6a
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-15 19:29:00 +0800

    commit

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
-
+    public function create() {
+        return view('auth.register');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthenticatedUserController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('guest')->group(function() {
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('/register', [RegisteredUserController::class, 'create']);
+
+    Route::get('/login', [AuthenticatedUserController::class, 'create'])->name('login');
+    Route::post('/login', [AuthenticatedUserController::class, 'create']);
+});
