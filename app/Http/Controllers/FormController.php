8ce1d594d60aaeced40ead885dc818ff221e3b98
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-16 21:22:31 +0800

    commit

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class FormController extends Controller
 {
@@ -13,5 +16,16 @@ class FormController extends Controller
     public function create()
     {
         return view('forms.create');
+    }
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'username' => ['required', 'string', 'max:255'],
+            'password' => ['required', Password::defaults()],
+        ]);
+        Auth::user()->forms()->create([
+            'username' => $validated['username'],
+            'password' => Hash::make($validated['password']),
+        ]);
+        return redirect()->route('forms.index');
     }
 }
