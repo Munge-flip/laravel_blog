d5bd81079f05c8a99d1f158c9a21fda0ef4212ad
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-20 21:31:22 +0800

    commit

---
+++ resources/views/forms/index.blade.php
@@ -0,0 +1,1 @@
+<a href="{{route('forms.create')}}">Add</a>
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthentitacedSessionController;
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+
+    Route::get('/login', [AuthentitacedSessionController::class, 'create'])->name('login');
+    Route::post('/login', [AuthentitacedSessionController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('forms', FormController::class);
+});
