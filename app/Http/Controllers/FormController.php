52f2ebe257f43249c66eb8a5b7cb1315dc195d91
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-23 09:27:12 +0800

    commit

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,18 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class FormController extends Controller
 {
     public function index()
     {
-        return view('forms.index');
+        $forms = Auth::user()->forms;
+        return view('forms.index', compact('forms'));
     }
     public function create()
     {
         return view('forms.create');
     }
-    public function store(Request $request) {
-        
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'username' => ['required', 'string', 'max:255'],
+            'password' => ['required', Password::defaults()],
+        ]);
+        Auth::user()->forms()->create([
+            'username' => $validated['username'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return redirect()->route('forms.index');
+    }
+    public function show($id)
+    {
+        $form = Auth::user()->forms()->findOrFail($id);
+
+        return view('forms.show', compact('form'));
+    }
+    public function edit($id)
+    {
+        $form = Auth::user()->forms()->findOrFail($id);
+
+        return view('forms.edit', compact('form'));
     }
 }
