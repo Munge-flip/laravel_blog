cc199dc0c85c0622b0d4fab1e2aff5cf6f8961a1
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-26 00:03:58 +0800

    commit

--- resources/views/forms/show.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <p><strong>ID: </strong> {{$form->id}} </p>
-    <p><strong>Username: </strong> {{$form->username}} </p>
-    <p><strong>Password: </strong> {{$form->password}} </p>
-    <p><strong>Date Created: </strong> {{$form->created_at->format('M d, Y')}} </p>
-    <a href="{{route('forms.index')}}">Back to the list</a>
-</div>
