f1913af418130083f816936b9e3e88f97a29e5f4
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-26 10:19:52 +0800

    commit

---
+++ resources/views/forms/create.blade.php
@@ -0,0 +1,34 @@
+Add
+
+<form method="POST" action="{{route('forms.store')}}">
+    @csrf
+    <div>
+        <label for="create-firstName">First Name</label><br>
+        <input type="text" name="firstName" id="create-firstName" value="{{old('firstName')}}">
+        @error('firstName')
+        <div style="color: red"> {{$message}} </div>
+        @enderror
+    </div>
+    <div>
+        <label for="create-lastName">Last Name</label><br>
+        <input type="text" name="lastName" id="create-lastName" value="{{old('lastName')}}">
+        @error('lastName')
+        <div style="color: red"> {{$message}} </div>
+        @enderror
+    </div>
+    <div>
+        <label for="create-middleName">Middle Name</label><br>
+        <input type="text" name="middleName" id="create-middleName" value="{{old('middleName')}}">
+        @error('middleName')
+        <div style="color: red"> {{$message}} </div>
+        @enderror
+    </div>
+    <div>
+        <label for="create-dateOfBirth">Middle Name</label><br>
+        <input type="date" name="dateOfBirth" id="create-dateOfBirth" value="{{old('dateOfBirth')}}">
+        @error('dateOfBirth')
+        <div style="color: red"> {{$message}} </div>
+        @enderror
+    </div>
+    <input type="submit" value="SUBMIT">
+</form>
